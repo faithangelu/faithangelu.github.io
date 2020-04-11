65a702c12c7d8caca03a446394cc7241e361ae3d
Hidalgo, Christine Faith Angelou A <faith.hidalgo@digify.com.ph>
2020-04-11 20:15:59 +0800

    * updates the sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -2,34 +2,15 @@
 $to = "imfaithangelou@gmail.com";
 $subject = $_POST('portfolio-contact-subject');
 
-$message = "
-<html>
-<head>
-<title>HTML email</title>
-</head>
-<body>
-<p>This email contains HTML Tags!</p>
-<table>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th>
-</tr>
-<tr>
-<td>John</td>
-<td>Doe</td>
-</tr>
-</table>
-</body>
-</html>
-";
+$message = $_POST('portfolio-contact-message');
 
 // Always set content-type when sending HTML email
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 // More headers
-$headers .= 'From: <webmaster@example.com>' . "\r\n";
-$headers .= 'Cc: myboss@example.com' . "\r\n";
+// $headers .= 'From: <webmaster@example.com>' . "\r\n";
+// $headers .= 'Cc: myboss@example.com' . "\r\n";
 
-mail($to,$subject,$message,$headers);
+mail($to,$subject,$message,$_POST('portfolio-contact-email'));
 ?>
